a3bc7896d7fd9de68917279199d29a1162d11011
moemen saadeh <moemen.gcbdev@gulfofficial.com>
2024-07-17 22:30:23 +0300

    new changes

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -21,14 +21,14 @@ const Collection = ({
   limit,
   page,
   totalPages = 0,
-}) => {
+}: CollectionProps) => {
   return (
     <>
       {data.length > 0 ? (
         <div className="flex flex-col items-center gap-10">
           <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
             {data.map((event: IEvent) => {
-              const hasOrderLInk = collectionType === "Events_Organized";
+              const hasOrderLInk = collectionType === "Event_Organized";
               const hidePrice = collectionType === "My_Tickets";
               return (
                 <li key={event._id} className="flex justify-center">
