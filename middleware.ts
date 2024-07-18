2f4fdea749b765cacd8c1b2f2aec728f7c128332
moemen saadeh <moemen.gcbdev@gulfofficial.com>
2024-07-18 23:29:03 +0300

    new changes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/events/create"]);
+const isProtectedRoute = createRouteMatcher([
+  "/events/create",
+  "/profile",
+  "/orders",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
